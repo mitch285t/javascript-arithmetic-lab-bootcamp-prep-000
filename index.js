428c4456fd5913d98fd6a00e141d28882974fe3b
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-14 21:54:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-A = math.floor(math.random() * 1000) 
+a = math.floor(math.random() * 1000) 
 b = math.floor(math.random() * 1000)
 
 function add(a, b){
-  return (A + b)
+  return (a + b)
   }
   
 function subtract(a, b) {
-  return (A - b)
+  return (a - b)
 }
 
 function multiply(a, b){
-  return (A * b)
+  return (a * b)
 }
 
 function divide(a, b){
-  return (A / b)
+  return (a / b)
 }
 function inc(a){
-  return (A += 1 )
+  return (a += 1 )
 }
 function dec(a){
-  return (A - 1)
+  return (a - 1)
 }
 function makeInt(a){
- return parseInt(A, 10) 
+ return parseInt(a, 10) 
 }
